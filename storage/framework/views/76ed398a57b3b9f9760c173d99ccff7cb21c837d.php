1146fbe08bf069b1067d4569fb6b519256240cf0
nasimrana69@gmail.com <nasimrana69@gmail.com>
2022-01-25 14:37:48 +0600

    merge with master

---
+++ storage/framework/views/76ed398a57b3b9f9760c173d99ccff7cb21c837d.php
@@ -0,0 +1,32 @@
+<?php $__env->startSection('content'); ?>
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show Product</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="<?php echo e(route('products.index')); ?>"> Back</a>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Name:</strong>
+                <?php echo e($product->name); ?>
+
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Details:</strong>
+                <?php echo e($product->detail); ?>
+
+            </div>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+<p class="text-center text-primary"><small>Nasim Rana.com</small></p>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/laravel-8-roles-and-permissions/resources/views/products/show.blade.php ENDPATH**/ ?>
